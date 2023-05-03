695ddf855e905ffcb2f6ac378bda6cfebf1c7c05
trand <kieutrungtran@gmail.com>
2023-05-02 20:55:13 -0400

    database

--- consumer.py
+++ consumer.py
@@ -1,4 +1,21 @@
 from kafka import KafkaConsumer
+import json
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///transactions.db')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+class Transaction(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    amount = Column(Float)
+
 
 # create a Kafka consumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'],
@@ -7,4 +24,23 @@ consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'],
 # continuously poll for new messages and print them
 for message in consumer:
     data = message.value
-    print(data['message'], data['count'])
+    print(data['name'], data['amount'])
+    transaction = Transaction(name=data['name'], amount=data['amount'])
+    session = Session()
+    session.add(transaction)
+    session.commit()
+
+# import sqlite3
+#
+# # Create a new database file
+# conn = sqlite3.connect('transactions.db')
+#
+# # Create a cursor object to execute SQL commands
+# cursor = conn.cursor()
+#
+# # Create a new table
+# cursor.execute('''CREATE TABLE users
+#                   (id INTEGER PRIMARY KEY, name TEXT, amount TEXT)''')
+#
+# # Close the connection
+# conn.close()
--- producer.py
+++ producer.py
@@ -7,8 +7,8 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
 # loop 20 times and send JSON messages to the 'test' topic
-for i in range(20):
-    data = {'message': f'Hello, World! {i}', 'count': i}
+for _ in range(10):
+    data = {'name': 'Alice', 'amount': '100.00'}
     producer.send('test', value=data)
     producer.flush()
     time.sleep(1)
