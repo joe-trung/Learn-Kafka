f86c921df842f654cde444c2562dcae629a8cd50
trand <kieutrungtran@gmail.com>
2023-05-07 21:13:19 -0400

    done

---
+++ createtable.py
@@ -0,0 +1,6 @@
+# import sqlite3
+#
+# conn = sqlite3.connect('transactions.db')
+# cursor = conn.cursor()
+# cursor.execute('''CREATE TABLE transaction2 (custid Integer Primary key,ac_type Text,date text,amt Float)''')
+# conn.close()
